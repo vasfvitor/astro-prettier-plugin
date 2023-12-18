e4f15ad999dd2f94475956b43b077ba6c303e101
vasfvitor <gitkey@virtuaires.com.br>
2023-12-17 23:38:50 -0300

    create failing test

--- test/tests/other.test.ts
+++ test/tests/other.test.ts
@@ -126,3 +126,5 @@ test(
 test('Can ignore self-closing elements', files, 'other/ignore-self-close');
 
 test('can format spread attributes', files, 'other/spread-attributes');
+
+test('Can format SVG', files, 'other/svg');
